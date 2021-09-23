40a24b5c6cb7adaff54c009575270acf29a32a5c
Reymina Nanaring <88993046+Rnanaring@users.noreply.github.com>
2021-09-23 02:38:22 -0700

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light" style="margin-top: 400px;">
+    <p class="text-center py-2 mb-0">Copyright © 2021 Designed by <a href="https://github.com/Rnanaring/Project.git">Group 3</a></p>
+</div>
